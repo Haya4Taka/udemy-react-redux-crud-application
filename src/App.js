34520d992ac48b18fb9b8302dd69cf9d8ca91548
林貴洋 <takahiro@MacBook-Pro.lan>
2020-07-06 17:27:25 +0900

    change App component from function component to class component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1 className="App">Hello, World!</h1>;
+  }
 }
 
 export default App;
