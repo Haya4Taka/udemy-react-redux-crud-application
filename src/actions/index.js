a20d9a76418a2f5db6f25b263a3f467a78aa4a99
林貴洋 <takahiro@MacBook-Pro.lan>
2020-07-06 20:57:06 +0900

    Inplement action

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+// action creator
+
+// return action object
+export const increment = () => ({
+  type: INCREMENT
+})
+
+// return action object
+export const decrement = () => ({
+  type: DECREMENT
+})
