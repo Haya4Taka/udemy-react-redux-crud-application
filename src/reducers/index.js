98066176f070b74952a02392e440fc7c32bce6f9
林貴洋 <takahiro@MacBook-Pro.local>
2020-07-07 16:21:00 +0900

    implement events index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 // reducerを総括するファイル
 import { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
